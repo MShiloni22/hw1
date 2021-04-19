718762a1573c91af6307cc7913056376d1bdf399
Matan Shiloni <mshiloni22@gmail.com>
2021-04-19 08:43:57 +0300

    done getbyte function + added comments

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int getByte(int x, int y);
 
 int main()
 {
-    /*int a, b;
+    /* int a, b;
     printf("choose two integers for swap function:\n");
     scanf("%d%d", &a, &b);
     swap(&a, &b);
@@ -16,16 +16,17 @@ int main()
     int x, y;
     printf("choose two integers for bitAnd function:\n");
     scanf("%d%d", &x, &y);
-    printf("result = %d\n", bitAnd(x, y)); */
+    printf("result = %d\n", bitAnd(x, y));
     int c, d;
     printf("choose two integers for getByte function:\n");
     scanf("%x%d", &c, &d);
-    printf("result = %x\n", getByte(c, d));
+    printf("result = %x\n", getByte(c, d)); */
     return 0;
 }
 
 
 void swap(int *a, int *b)
+// without temp variable
 {
     *a = *a^*b;
     *b = *b^*a;
@@ -34,12 +35,24 @@ void swap(int *a, int *b)
 
 
 int bitAnd(int x, int y)
+/*
+* bitAnd ‐ x&y using only ~ and |
+* Example: bitAnd(6,5) = 4
+* Legal ops: ~ |
+* Max ops: 8 */
 {
     return (~(~x|~y));
 }
 
 
 int getByte(int x, int y)
+/*
+* getByte ‐ Extract byte n from word x
+* Bytes numbered from 0 (LSB) to 3 (MSB)
+* Examples: getByte(0x12345678,1) = 0x56
+* Legal ops: ! ~ & ^ | + << >>
+* Max ops: 6
+*/
 {
     int mask = ((1 << 8) - 1);
     x = x >> (y << 3);
